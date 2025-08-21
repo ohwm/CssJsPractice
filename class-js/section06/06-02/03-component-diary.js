dd27d078bd6c0e1b4bd39dba9fc1b65c07d1f7af
ohwm <ohwmdev@gmail.com>
2025-08-21 12:51:04 +0000

    실습 6 완료 - 컴포넌트 사용

---
+++ class-js/section06/06-02/03-component-diary.js
@@ -0,0 +1,13 @@
+const diarymenu = `
+    <div>일기목록 메인화면 입니다.<div/>
+    <div>일기1<div/>
+    <div>일기2<div/>
+    <div>...<div/>
+`
+
+const dogmenu = `
+    <div>강아지목록 메인화면 입니다.<div/>
+    <div>강아지1<div/>
+    <div>강아지2<div/>
+    <div>...<div/>
+`
